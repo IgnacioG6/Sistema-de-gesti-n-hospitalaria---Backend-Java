ba5cac1bb6efdb2c86a023e07aab4c4a77e80371
Ignacio <ignaciog1459@outlook.com>
2026-01-15 17:06:29 -0300

    feat(service): implementar la clase DoctorService

--- src/main/java/com/example/hospital/service/DoctorService.java
+++ src/main/java/com/example/hospital/service/DoctorService.java
@@ -1,11 +1,11 @@
 package com.example.hospital.service;
 
+import com.example.hospital.dto.request.DoctorRequestDTO;
 import com.example.hospital.dto.response.DoctorResponseDTO;
 import com.example.hospital.exception.EntityNotFoundException;
 import com.example.hospital.mapper.DoctorMapper;
 import com.example.hospital.model.Doctor;
-import com.example.hospital.model.Paciente;
-import org.apache.catalina.mapper.Mapper;
+import com.example.hospital.model.enums.Especialidad;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -21,24 +21,84 @@ public class DoctorService {
                 .toList();
     }
 
+    public Doctor buscarEntidadPorId(Long id) {
+        return doctores.stream()
+                .filter(d -> d.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Doctor no encontrado con id: " + id)
+                );
+    }
+
 
     public DoctorResponseDTO buscarDoctorPorId(Long id) {
-        for (Doctor doctor : doctores) {
-            if (doctor.getId().equals(id)) {
-                return DoctorMapper.toResponseDTO(doctor);
-            }
-        }
-        throw new EntityNotFoundException("Doctor no encontrado con id: " + id);
+        Doctor doctor = buscarEntidadPorId(id);
+        return DoctorMapper.toResponseDTO(doctor);
     }
 
     public DoctorResponseDTO buscarDoctorPorLicencia(String licencia) {
-        for (Doctor doctor : doctores) {
-            if (doctor.getLicenciaMedica().equals(licencia)) {
-                return DoctorMapper.toResponseDTO(doctor);
-            }
+        if (licencia == null || licencia.isBlank()) {
+            throw new IllegalArgumentException("La licencia médica es obligatoria");
         }
-        throw new EntityNotFoundException("Doctor no encontrado con licencia: " + licencia);
+
+        return doctores.stream()
+                .filter(doctor -> licencia.equals(doctor.getLicenciaMedica()))
+                .findFirst()
+                .map(DoctorMapper::toResponseDTO)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Doctor no encontrado con licencia: " + licencia
+                ));
     }
+
+    public DoctorResponseDTO registarDoctor(DoctorRequestDTO doctorRequestDTO) {
+        Doctor doctor = new Doctor();
+        doctor.setNombre(doctorRequestDTO.nombre());
+        doctor.setLicenciaMedica(doctorRequestDTO.licenciaMedica());
+        doctor.setEspecialidad(doctorRequestDTO.especialidad());
+        doctor.setEmail(doctorRequestDTO.email());
+        doctor.setTelefono(doctorRequestDTO.telefono());
+        doctor.setAñosExperiencia(doctorRequestDTO.añosExperiencia());
+        doctor.setHorarioAtencion(doctorRequestDTO.horarioAtencion());
+        doctor.setDisponible(true);
+
+        doctores.add(doctor);
+        return DoctorMapper.toResponseDTO(doctor);
+    }
+
+
+    public DoctorResponseDTO actualizarDoctor(Long id, DoctorRequestDTO dto){
+        Doctor doctor = buscarEntidadPorId(id);
+
+        doctor.setNombre(dto.nombre());
+        doctor.setLicenciaMedica(dto.licenciaMedica());
+        doctor.setEspecialidad(dto.especialidad());
+        doctor.setTelefono(dto.telefono());
+        doctor.setEmail(dto.email());
+        doctor.setAñosExperiencia(dto.añosExperiencia());
+        doctor.setHorarioAtencion(dto.horarioAtencion());
+
+        return DoctorMapper.toResponseDTO(doctor);
+    }
+
+    public void eliminarDoctor(Long id) {
+        Doctor doctor = buscarEntidadPorId(id);
+        doctores.remove(doctor);
+    }
+
+    public DoctorResponseDTO cambiarDisponibilidad(Long id, boolean disponible){
+        Doctor doctor =  buscarEntidadPorId(id);
+        doctor.setDisponible(disponible);
+        return DoctorMapper.toResponseDTO(doctor);
+    }
+
+    public List<DoctorResponseDTO> buscarPorEspecialidad(Especialidad especialidad){
+        return doctores.stream()
+                .filter(doctor -> doctor.getEspecialidad().equals(especialidad))
+                .map(DoctorMapper::toResponseDTO)
+                .toList();
+    }
+
+
 
 
 }
