86950f9697be7be2e9099eb483d9461fe396f4d8
Ignacio <ignaciog1459@outlook.com>
2026-01-21 22:59:04 -0300

    feat(controller): implementar RecetaController y endpoints de gestión

--- src/main/java/com/example/hospital/controller/RecetaController.java
+++ src/main/java/com/example/hospital/controller/RecetaController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/recetas")
 public class RecetaController {
@@ -25,9 +27,27 @@ public class RecetaController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<RecetaResponseDTO> buscarRecetaPorId(Long id) {
+    public ResponseEntity<RecetaResponseDTO> buscarRecetaPorId(@PathVariable Long id) {
         RecetaResponseDTO receta = recetaService.buscarRecetaPorId(id);
         return  ResponseEntity.status(HttpStatus.OK).body(receta);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RecetaResponseDTO>> listarRecetas() {
+        return ResponseEntity.status(HttpStatus.OK).body(recetaService.listarRecetas());
+    }
+
+
+    @GetMapping("/pacientes/{id}")
+    public ResponseEntity<List<RecetaResponseDTO>> buscarRecetaPorIdPaciente(@PathVariable Long id) {
+        List<RecetaResponseDTO> recetas = recetaService.buscarRecetaPorPaciente(id);
+        return ResponseEntity.status(HttpStatus.OK).body(recetas);
+    }
+
+    @GetMapping("/historias-clinicas/{id}")
+    public ResponseEntity<List<RecetaResponseDTO>> buscarRecetaPorHistoriaClinica(@PathVariable Long id) {
+        List<RecetaResponseDTO> receta = recetaService.buscarRecetaPorHistoriaClinica(id);
+        return ResponseEntity.status(HttpStatus.OK).body(receta);
     }
 
 
---
+++ src/main/java/com/example/hospital/dto/response/FacturaResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.hospital.dto.response;
+
+public record FacturaResponseDTO() {
+}
